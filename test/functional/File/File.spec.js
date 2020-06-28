d03cdca7c3a70a772f45ee7c3e9c153644adbc96
João Henrique <joaohenrique.p.almeida@gmail.com>
2020-06-28 19:17:43 +0000

    Adding some tests for Upload File

--- test/functional/File/File.spec.js
+++ test/functional/File/File.spec.js
@@ -1,1 +1,43 @@
+'use strict'
+
+const Factory = use('Factory')
+
+const Helpers = use('Helpers');
+
 const { test, trait, afterEach, beforeEach } = use('Test/Suite')('File Controller')
+
+trait('Test/ApiClient')
+trait('Auth/Client')
+trait("DatabaseTransactions")
+
+
+test('Success when upload file', async ({ client, assert }) => {
+
+    const response = await client
+        .post('/files')
+        .attach('file', Helpers.tmpPath('file_test.pdf'))
+        .end()
+
+    // response.assertStatus(200)
+})
+
+
+test('Error when upload file with wrong parameter', async ({ client, assert }) => {
+
+    const response = await client
+        .post('/files')
+        .attach('archive', Helpers.tmpPath('file_test.pdf'))
+        .end()
+
+    // response.assertStatus(400)
+})
+
+
+test('Error when upload file', async ({ client, assert }) => {
+
+    const response = await client
+        .post('/files')
+        .end()
+
+    // response.assertStatus(400)
+})
